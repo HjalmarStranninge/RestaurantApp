5abc36d3b2bbdd9d66bb525b6afed77763f11e42
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-08-26 16:08:46 +0200

    Outlined all endpoints I'll need.

--- RestaurantApp/Controllers/CustomerController.cs
+++ RestaurantApp/Controllers/CustomerController.cs
@@ -7,5 +7,26 @@ namespace RestaurantApp.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
+        [HttpPost]
+        public async Task<ActionResult> CreateCustomer()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteCustomer()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpdateCustomerInfo()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllCustomers()
+        {
+            return Ok();
+        }
     }
 }
