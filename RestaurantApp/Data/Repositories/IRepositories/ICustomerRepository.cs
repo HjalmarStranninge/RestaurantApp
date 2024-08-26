35f4309af43b08fe004d1e6a8bf87a47919697d3
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-08-26 16:12:25 +0200

    Created interface for CustomerRepository
    - Added all functions needed.

---
+++ RestaurantApp/Data/Repositories/IRepositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantApp.Models;
+
+namespace RestaurantApp.Data.Repositories.IRepositories
+{
+    public interface ICustomerRepository
+    {
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task CreateCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int customerId);
+    }
+}
