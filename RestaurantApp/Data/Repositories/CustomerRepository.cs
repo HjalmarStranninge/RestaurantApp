2a0498f044d6100b2b404d662b097dba53ce1b82
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-08-26 16:14:32 +0200

    Added CustomerDTO

---
+++ RestaurantApp/Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,28 @@
+﻿using RestaurantApp.Data.Repositories.IRepositories;
+using RestaurantApp.Models;
+
+namespace RestaurantApp.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public Task CreateCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustomerAsync(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
