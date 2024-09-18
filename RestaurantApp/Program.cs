d7eee2c467b876db29d986f4740b040260202db1
Hjalmar Stranninge <Hjalmar.stranninge@outlook.com>
2024-09-18 09:59:44 +0200

    Added CORS config

--- RestaurantApp/Program.cs
+++ RestaurantApp/Program.cs
@@ -19,16 +19,31 @@ builder.Services.AddDbContext<RestaurantAppContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("LocalReact", policy =>
+    {
+        policy.WithOrigins("https://localhost:7185/")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<ICustomerService, CustomerService>();
+
 builder.Services.AddScoped<ITableRepository, TableRepository>();
 builder.Services.AddScoped<ITableService, TableService>();
+
 builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 builder.Services.AddScoped<IBookingService, BookingService>();
+
 builder.Services.AddScoped<IMenuItemRepository, MenuItemRepository>();
 builder.Services.AddScoped<IMenuItemService, MenuItemService>();
 
 var app = builder.Build();
+
+app.UseCors("LocalReact");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
